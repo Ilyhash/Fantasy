366c2e264af06f961ccd793628ffda01d5e91e79
Ilya <astfgl96@gmail.com>
2022-07-22 18:06:16 +0300

    .

--- Fantasy.py
+++ Fantasy.py
@@ -12,11 +12,11 @@ def special_case(fighter1, fighter2):
     if fighter1 == Ch.Frodo or fighter2 == Ch.Frodo:
         c = randint(1, 5)
         if c == 5 and fighter1 == Ch.Frodo:
-            print('Frodo used the ring and killed {} \n{} is dead'.format(fighter2.name, fighter2.name))
+            print(f'Frodo used the ring and killed {fighter2.name} \n{fighter2.name} is dead')
             fighter2.health = 0
             return
         elif c == 5 and fighter2 == Ch.Frodo:
-            print('Frodo used the ring and killed {} \n{} is dead'.format(fighter1.name, fighter1.name))
+            print(f'Frodo used the ring and killed {fighter1.name} \n{fighter1.name} is dead')
             fighter1.health = 0
             return
     if fighter1 == Ch.Horus and fighter2 == Ch.TheEmperor or fighter1 == Ch.TheEmperor and fighter2 == Ch.Horus:
@@ -27,22 +27,21 @@ def special_case(fighter1, fighter2):
         print('{} dies'.format(Ch.Boromir.name))
     if fighter1 == Ch.ChaosKnight or fighter2 == Ch.ChaosKnight:
         r = randint(1, 5)
-        Ch.ChaosKnight.str = randint(Ch.Knight.str - r, Ch.Knight.str + r)
-        Ch.ChaosKnight.agi = randint(Ch.Knight.agi - r, Ch.Knight.agi + r)
-        Ch.ChaosKnight.int = randint(Ch.Knight.int - r, Ch.Knight.int + r)
+        Ch.ChaosKnight.str = randint(Ch.ChaosKnight.str - r, Ch.ChaosKnight.str + r)
+        Ch.ChaosKnight.agi = randint(Ch.ChaosKnight.agi - r, Ch.ChaosKnight.agi + r)
+        Ch.ChaosKnight.int = randint(Ch.ChaosKnight.int - r, Ch.ChaosKnight.int + r)
         Ch.ChaosKnight.min_atk = randint(1, 5)
         Ch.ChaosKnight.max_atk = Ch.ChaosKnight.min_atk + randint(0, 2)
 
 
 def death_confirm(fighter1, fighter2):
-    while fighter1.health <= 0 or fighter2.health <= 0:
+    if fighter1.health <= 0 or fighter2.health <= 0:
         if fighter1.health <= 0 and fighter2.health <= 0:
             print('Both are dead')
         elif fighter1.health <= 0:
-            print('{} is dead.'.format(fighter1.name))
-        elif fighter2.health <= 0:
-            print('{} is dead.'.format(fighter2.name))
-        break
+            print(f'{fighter1.name} is dead.')
+        else:
+            print(f'{fighter2.name} is dead.')
 
 
 def atk(self):
@@ -59,7 +58,7 @@ def stat_choice(fighter1, fighter2):
     elif c == 2:
         par1 = fighter1.agi
         par2 = fighter2.agi
-    elif c == 3:
+    else:
         par1 = fighter1.int
         par2 = fighter2.int
     return [par1, par2]
@@ -70,11 +69,11 @@ def fighting():
     fighter2 = fighter_choice()
     while fighter1 == fighter2:
         fighter2 = fighter_choice()
-    print('{} {}'.format(fighter1.name, fighter1.desc))
+    print(f'{fighter1.name}, {fighter1.desc}')
     time.sleep(1)
     print('vs')
     time.sleep(1)
-    print('{} the {} \n'.format(fighter2.name, fighter2.desc))
+    print(f'{fighter2.name}, {fighter2.desc}')
     time.sleep(1)
     special_case(fighter1, fighter2)
     while fighter1.health > 0 and fighter2.health > 0:
@@ -82,19 +81,17 @@ def fighting():
         if par1 > par2:
             attack = atk(fighter1)
             fighter2.health = fighter2.health - attack if fighter2.health >= attack else 0
-            print('{} gets hit for {} dmg, health is {}'.format(fighter2.name, attack, fighter2.health))
+            print(f'{fighter2.name} gets hit for {attack} dmg, health is {fighter2.health}')
         elif par1 < par2:
             attack = atk(fighter2)
             fighter1.health = fighter1.health - attack if fighter1.health >= attack else 0
-            print('{} gets hit for {} dmg, health is {}'.format(fighter1.name, attack, fighter1.health))
+            print(f'{fighter1.name} gets hit for {attack} dmg, health is {fighter1.health}')
         else:
             attack1 = atk(fighter1)
             attack2 = atk(fighter2)
             fighter1.health = fighter1.health - attack2 if fighter1.health >= attack2 else 0
             fighter2.health = fighter2.health - attack1 if fighter2.health >= attack1 else 0
-            print("Both get hit, {}'s health is {} and {}'s health is {}".format(fighter1.name, fighter1.health,
-                                                                                 fighter2.name,
-                                                                                 fighter2.health))
+            print(f'Both get hit, {fighter1.name}\'s health is {fighter1.health} and {fighter2.name}\'s health is {fighter2.health}')
         time.sleep(1)
         death_confirm(fighter1, fighter2)
 
